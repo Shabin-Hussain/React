47e18b88e865315d5c1f25dee83aba328bd22f11
Shabin-Hussain <shabinhussain112@gmail.com>
2026-08-21 19:08:24 +0530

    new

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import myStyle from '../src/myStyle.module.css'
+import Random from './Random';
+import Example from './Example';
 
 function App() {
+  //js code
+  const h1style= {color:'green',backgroundColor:'yellow',fontSize:'70px'}
+
+  const subject='REACT'
   return (
+    //html code
   <>
       <div>
-         <h1>Hi</h1>
+         <h1 className={myStyle.reactStyle}>Hi</h1>
       </div>
-      <h2 style={{color:'green',backgroundColor:'yellow'}}>hello</h2>
+      <h2 style={h1style}>hello</h2>
+      <p className={myStyle.reactStyleT}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eum dolorem est! Omnis fugiat reprehenderit aspernatur ab cupiditate eius sed, sapiente voluptatibus cum, obcaecati adipisci consequuntur ea suscipit ducimus sit.</p>
+
+      <h1 className={[myStyle.style2,myStyle.style3].join(' ')}>components</h1>
+      <h1>My content is :{subject}</h1>
+
+      <ul>
+        <li><Random/></li>
+        <li><Example/></li>
+      </ul>
       
   </>
   );

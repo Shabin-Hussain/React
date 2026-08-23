737f04d3a2b18baab9dca17845a9b496911809e3
Shabin-Hussain <shabinhussain112@gmail.com>
2026-08-23 14:25:26 +0530

    new

--- demo/src/Random.jsx
+++ demo/src/Random.jsx
@@ -5,13 +5,27 @@ export default class Random extends Component {
   constructor(props){
     super(props)
     console.log(props);
-    
+
+    this.state = {cname:'Auto'}
+
   }
+
+
+  change(data){
+     this.setState({cname:data})
+  }
+
+
+
+
   render() {
     return (
      <> 
      <div>Random</div>
      <p>data shared is: {this.props.uname}</p>
+     <p>car is : {this.state.cname}</p>
+
+     <button className='btn btn-primary' onClick={()=>this.change('swift')}>Click</button>
      </>
     )
   }

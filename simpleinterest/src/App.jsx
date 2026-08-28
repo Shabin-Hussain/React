0d55f0b9c7ccdab49c1f6918dec5e0a172be1bed
Shabin-Hussain <shabinhussain112@gmail.com>
2026-08-28 18:38:28 +0530

    new

--- simpleinterest/src/App.jsx
+++ simpleinterest/src/App.jsx
@@ -12,6 +12,71 @@ function App() {
   const [year, setYear] = useState(0)
   const [interest, setInterest] = useState(0)
 
+  /* for conditional rendering */
+  const [isPrinciple, setIsPrinciple] = useState(true)
+  const [isRate, setIsRate] = useState(true)
+  const [isYear, setIsYear] = useState(true)
+
+  const validate = (e) => {
+    /*  console.log(e.target.value);
+     console.log(e.target.name); */
+
+    let name = e.target.name
+    let value = e.target.value
+    console.log(!!value.match(/^[0-9]*$/));
+
+    if (!!value.match(/^[0-9]*$/)) {
+      if (name == 'principle') {
+        setPrinciple(value)
+        setIsPrinciple(true)
+      }
+      else if (name == 'rate') {
+        setRate(value)
+        setIsRate(true)
+      }
+      else {
+        setYear(value)
+        setIsYear(true)
+      }
+
+    }
+    else {
+      if (name == 'principle') {
+        setPrinciple(value)
+        setIsPrinciple(false)
+      }
+      else if (name == 'rate') {
+        setRate(value)
+        setIsRate(false)
+      }
+      else {
+        setYear(value)
+        setIsYear(false)
+      }
+
+    }
+
+
+  }
+
+  const handleReset = () => {
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+    setIsPrinciple(true)
+    setIsRate(true)
+    setIsYear(true)
+    setInterest(0)
+  }
+
+  const calculate=()=>{
+    setInterest((principle*rate*year)/100)
+  }
+  /* 
+    console.log('principle', principle);
+    console.log('rate', rate);
+    console.log('year', year);
+   */
 
   return (
     <div className='d-flex justify-content-center align-items-center' style={{ width: '100%', height: '100vh' }}>
@@ -20,26 +85,32 @@ function App() {
         <p>Calculate your simple interest easily</p>
 
         <div className='mt-5 d-flex bg-warning justify-content-center align-items-center rounded shadow flex-column p-4'>
-          <h2 className='fs-1 fw-bolder'>₹ 0</h2>
+          <h2 className='fs-1 fw-bolder'>₹ {interest}</h2>
           <p>Total simple interest</p>
         </div>
 
         <form className='mt-5'>
           <div className='mb-3'>
-            <TextField id="outlined-basic" label="₹ Principle amount" variant="outlined" className='w-100' />
+            <TextField id="outlined-basic" value={principle || ""} label="₹ Principle amount" variant="outlined" className='w-100' name='principle' onChange={(e) => validate(e)} />
+            {!isPrinciple &&
+              <p className='text-danger'>*Invalid Input</p>}
 
           </div>
           <div className='mb-3'>
-            <TextField id="outlined-basic" label="Rate of Interest (p.a)%" variant="outlined" className='w-100' />
+            <TextField id="outlined-basic" value={rate || ""} label="Rate of Interest (p.a)%" variant="outlined" className='w-100' name='rate' onChange={(e) => validate(e)} />
+            {!isRate &&
+              <p className='text-danger'>*Invalid Input</p>}
           </div>
           <div className='mb-3'>
-            <TextField id="outlined-basic" label="Year (Yr)" variant="outlined" className='w-100' />
+            <TextField id="outlined-basic" value={year || ""} label="Year (Yr)" variant="outlined" className='w-100' name='year' onChange={(e) => validate(e)} />
+            {!isYear &&
+              <p className='text-danger'>*Invalid Input</p>}
           </div>
 
           <div className="d-flex justify-content-between w-100 mt-4">
 
-            <Button variant="contained" color='success' style={{ width: '190px', height: '60px' }}>Calculate</Button>
-            <Button variant="outlined" style={{ width: '190px', height: '60px' }}>Reset</Button>
+            <Button variant="contained" color='success' disabled={isPrinciple && isRate && isYear ? false:true} style={{ width: '190px', height: '60px' }} onClick={calculate}>Calculate</Button>
+            <Button variant="outlined" style={{ width: '190px', height: '60px' }} onClick={handleReset}>Reset</Button>
           </div>
         </form>
 

fe0dfb5e9821787a1368cd09fae12fcdac89574b
Shabin-Hussain <shabinhussain112@gmail.com>
2026-08-30 21:23:22 +0530

    new

--- mediaplayer/src/App.jsx
+++ mediaplayer/src/App.jsx
@@ -1,11 +1,21 @@
-
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Landingpage from './pages/Landingpage'
+import Home from './pages/Home'
+import Watchhistory from'./pages/Watchhistory'
 
 function App() {
 
 
   return (
     <>
+    {/* / - base url */}
+    <Routes>
+      <Route path='/' element={<Landingpage/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/watch-history' element={<Watchhistory/>}/>
+
+    </Routes>
       
     </>
   )
